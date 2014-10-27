ba1edae6a42b94e18b96e785345211aa5557bc1f
XMJ <xmj@XMJ.local>
2014-10-27 10:05:38 +0800

    Initial Commit

---
+++ 飞机游戏/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  飞机游戏
+//
+//  Created by XMJ on 14-10-27.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 飞机游戏/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  飞机游戏
+//
+//  Created by XMJ on 14-10-27.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
